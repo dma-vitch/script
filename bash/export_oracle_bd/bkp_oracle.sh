ed669a4a1328a87de9ffd6aa41266ce007151e3d
dma-vitch <for_registration_forum@mail.ru>
2015-08-17 18:21:07 +0300

    add check for entering arguments
    file exist  and count arguments

--- bash/export_oracle_bd/bkp_oracle.sh
+++ bash/export_oracle_bd/bkp_oracle.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
+
+#check entering count arguments
+if [[  $# -lt 2 || $# -gt 3 ]]; then
+	echo "Usage: $0 bd_name IP_adress port(by default 1521)"
+	exit
+fi
+
 ##get Oracle Env Settings
-source ./envoracle
+if [ -s "./envoracle.cfg" ]; then
+    source ./envoracle.cfg
+else
+	echo "ERROR: file envoracle.cfg not exist,please set environment for oracle"
+fi
 
 #set date for backup
 dt=`date +%F`
 
 #get parameter
 bdname=$1
-host=$2
-port=$3
+iphost=$2
+dport=1521
+port=${3:-$dport}
 
-#set backup dir
+#set backup directory
 bkpdir=/opt/oracledump
 
-expdp \"sys/sys@$host:$port/$bdname AS SYSDBA\" dumpfile=$bdname-$dt.dmp directory=DUMPS LOGFILE=$bdname-$dt.log SCHEMAS=\(GUVD_ADDR_IMP,GUVD_ADDR,GUVD_ADDR_DT,GUVD_COMMONS,GUVD_EEST,GUVD_ENTITY_RELATIONS,GUVD_JES,GUVD_NGENIE,GUVD_PARTY,GUVD_SECURITY,GUVD_TASK,GUVD_USER,GUVD_WORKS\)
+expdp \"sys/sys@$iphost:$port/$bdname AS SYSDBA\" dumpfile=$bdname-$dt.dmp directory=DUMPS LOGFILE=$bdname-$dt.log SCHEMAS=\(GUVD_ADDR_IMP,GUVD_ADDR,GUVD_ADDR_DT,GUVD_COMMONS,GUVD_EEST,GUVD_ENTITY_RELATIONS,GUVD_JES,GUVD_NGENIE,GUVD_PARTY,GUVD_SECURITY,GUVD_TASK,GUVD_USER,GUVD_WORKS\)
 # run sql from shell to clone PDB
-#sqlplus sys/sys@192.168.58.102:1521/$bdname @clone_pdb.sql
+#sqlplus sys/sys@$iphost:$port/$bdname @clone_pdb.sql
 if [[ $? != 0 ]]; then
     echo -e "Failed backup of $bdname!"
 else
-    find $bkpdir/ -type f \( -name "$bdname-*.dmp" -o -name "bdname-*.log" \) -mtime +2 #| xargs rm -f;
+    find $bkpdir/ -type f \( -name "$bdname-*.dmp" -o -name "bdname-*.log" \) -mtime +2 | xargs rm -f;
 fi
