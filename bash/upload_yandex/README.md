43a3da38a3b0efb8d311587a8016de710578aa7b
dma-vitch <for_registration_forum@mail.ru>
2015-10-30 17:20:29 +0300

    add script for upload yandex disk

---
+++ bash/upload_yandex/README.md
@@ -0,0 +1,29 @@
+upload_yandex
+-------------
+
+Requirement
+------------
+
+-Install curl
+
+Features
+---------
+Upload files in yandex disk
+
+Usage
+-----
+
+- Set your variables in:
+```
+BK_DIR - path to files for upload 
+creditFile - name of file for autentification
+```
+- In creditFile you fill your credit for access to yandex disk:
+```
+username=login
+password=pass
+```
+- If you want upload into define directory, you will change this string
+` 
+https://webdav.yandex.ru/{directory}
+`
---
+++ bash/upload_yandex/upload_yandex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TIME=`date +%Y-%b`
+BK_DIR=/var/backups/foswiki/
+creditFile=credits.ini
+
+#crypt ypy files
+#GPG_COMMAND="gpg -c -z 0 --batch --passphrase XXXXXXXXXX"
+
+#check credit file
+if [ ! -f $creditFile ]; then
+        echo "please fill $creditFile (file has just been created)"
+        echo "username=$login
+password=$pass">$creditFile
+        exit 1
+fi
+
+f=`find $BK_DIR -mtime 0 -name *.tar.gz`
+echo "upload this files $f" > /tmp/yanlog.upload.txt
+. credits.ini
+
+#MYSQL_FILE=/tmp/$-mysql.$TIME.sql.gz
+
+# Archiving databases
+#mysqldump -u root --password=qwerty --all-databases | gzip > $MYSQL_FILE
+
+for file in $f
+do
+# Uploading to the cloud
+mime_type=`file --brief --mime-type "$file"`
+curl -Sv -k --user $username:"$password" -T -H "Content-Type: $mime_type" "$file" https://webdav.yandex.ru/123/ > /tmp/yanlog.upload.txt
+done
