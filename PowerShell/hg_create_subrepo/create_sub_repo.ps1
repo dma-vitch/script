41cb80ec9d93bb2a6b2a553cc9116f91955f5cf5
dma-vitch <for_registration_forum@mail.ru>
2015-11-27 12:22:48 +0300

    Add script for create sub repo mercurial

---
+++ PowerShell/hg_create_subrepo/create_sub_repo.ps1
@@ -0,0 +1,22 @@
+﻿# Project and relative paths as script parameters
+param([string]$project, [string]$relPath)
+
+# Let's see if there is an item .hg. If not, report error and quit
+if((test-path ".hg") -eq $false ) {
+   "You MUST run this at the top of your directory structure and use relative paths"
+    return
+}
+
+# Call Mercury
+& hg clone $project $relPath
+
+# Add data to .hgsub using composite formatting string
+add-content -path ".hgsub" -value $("{0} = {1}" -f $relPath, $project)
+
+# Check that .hgsub exists and issue Mercury commands if it does
+if(test-path ".hgsub") {
+    hg add .hgsub
+    hg commit
+} else {
+    "failure, see error messages above"
+}
