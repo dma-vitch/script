f816b7edbc8aeb3ec83149c3b9668cf455091c85
dma-vitch <for_registration_forum@mail.ru>
2016-03-24 11:22:03 +0300

    fix

--- bash/hdp/hdfs/fix_under_block_replica.sh
+++ bash/hdp/hdfs/fix_under_block_replica.sh
@@ -1,4 +1,9 @@
 ﻿#!/bin/bash
-su - hdfs 
-hdfs fsck / | grep 'Under replicated' | awk -F':' '{print $1}' >> /tmp/under_replicated_files 
-for hdfsfile in `cat /tmp/under_replicated_files`; do echo "Fixing $hdfsfile :" ; hadoop fs -setrep 3 $hdfsfile; done
+#su - hdfs
+replica=${1:-3}
+
+hdfs hdfs fsck / | grep 'Under replicated' | awk -F':' '{print $1}' >> /tmp/under_replicated_files
+for hdfsfile in `cat /tmp/under_replicated_files`
+do
+    echo "Fixing $hdfsfile :" hadoop fs -setrep $replica $hdfsfile
+done
