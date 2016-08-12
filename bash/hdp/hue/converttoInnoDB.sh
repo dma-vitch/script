5fd5b79532f76aceb554702a0f39dda45fb42d6a
Ilya Shataev <ilya.shataev@gmail.com>
2016-08-12 15:48:06 +0300

    add convert to InnoDB

---
+++ bash/hdp/hue/converttoInnoDB.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+# convert 
+echo -n "Enter database username: "
+read dbuser
+echo -n "Enter the MySQL password: "
+read -s rootpw
+echo -n "Enter database name: "
+read dbname
+ 
+#dbname=${1:-hue}
+mysql -u $dbuser -p$rootpw $dbname -e "show table status where Engine !='InnoDB';" | awk 'NR>1 {print "ALTER TABLE "$1" ENGINE = InnoDB;"}' | mysql -u $dbuser -p$rootpw $dbname
