dad8586565c9ab6546a67361c9cfb4264d80a0ca
Ilya Shataev <ilya.shataev@gmail.com>
2016-08-17 14:38:38 +0300

    init sync zabbix repo

---
+++ bash/repo/sync-zabbix.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+#
+# reposync Zabbix
+#
+
+distrelease=$(rpm -q --qf "%{version}" -f /etc/redhat-release)
+
+if [ ! -f /etc/yum/repos.d/zabbix.repo ]
+then
+    rpm -ivh http://repo.zabbix.com/zabbix/3.0/rhel/$distrelease/$(arch)/zabbix-release-3.0-1.el6.noarch.rpm
+fi
+
+BASEDIR=/var/www/html/zab
+mkdir -p $BASEDIR
+cd $BASEDIR
+reposync -n -r zabbix
+reposync -n -r zabbix-non-supported
+repomanage -o -c zabbix | xargs rm -fv
+#createrepo zabbix
+#createrepo zabbix-non-supported
+#update repo
+#createrepo --update zabbix
+#createrepo --update zabbix-non-supported
+
+chmod -R o-w+r $BASEDIR
